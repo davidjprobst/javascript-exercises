9ce3ccd69a0bace22cfca3e466612b7954059744
davidjprobst <probst.dj2@gmail.com>
2025-09-16 08:03:58 -0400

    Complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+// My initial solution
+const reverseString = function(string) {
+    let reversedString = "";
 
+    array = Array.from(string);
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString += array[i];
+    };
+
+    return reversedString;
+
+};
+
+// Solution using only JavaScript array methods
+const simplifiedReverseString = function(string) {
+    string.split('').reverse().join('');
 };
 
 // Do not edit below this line
