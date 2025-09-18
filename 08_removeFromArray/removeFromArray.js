822e9550c9c7c23aca6555a26f1b65ebd9e9dc7f
davidjprobst <probst.dj2@gmail.com>
2025-09-18 07:27:33 -0400

    My solution before reviewing provided solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...nums) {
+    for (num of nums) {
+        let currentNum = num;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === currentNum) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 };
+
+removeFromArray([1, 2, 2, 4], 2, 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
