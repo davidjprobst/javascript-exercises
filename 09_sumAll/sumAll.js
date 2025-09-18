aa874abda3e65bf0b8af541f02ec99438d1831e4
davidjprobst <probst.dj2@gmail.com>
2025-09-18 08:43:34 -0400

    complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+function sumAll(...args) {
+  const sortedNums = Array.from(args).sort();
+  const finalNum = sortedNums[sortedNums.length - 1];
+  let currentNum = sortedNums[0];
+  let sum = 0;
 
-};
+  for (num of sortedNums) {
+    if (num < 0) return "ERROR";
+    if (!Number.isInteger(num)) return "ERROR";
+  }
+  
+  while (currentNum <= finalNum) {
+     sum = sum + currentNum;
+     currentNum = currentNum + 1;
+  } return sum;
+  
+}
 
 // Do not edit below this line
 module.exports = sumAll;
