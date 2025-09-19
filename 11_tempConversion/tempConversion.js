ba740369125f3641820192d4a046e7d6cf63f93a
davidjprobst <probst.dj2@gmail.com>
2025-09-19 07:40:56 -0400

    complete 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  // x °F ≘ (x − 32) × ⁠5/9°C
+  let cTemp = 0;
+  cTemp = (fTemp - 32) * (5/9);
+  return Math.round(cTemp*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  // x °C ≘ (x × ⁠9/5 + 32) °F
+  let fTemp = 0;
+  fTemp = cTemp * (9/5) + 32;
+  return Math.round(fTemp*10)/10;
 };
 
 // Do not edit below this line
